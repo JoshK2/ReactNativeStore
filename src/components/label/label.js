ae26d2b014fc5976e8bd3ebbbcd5d2d4de36f72a
Josh Kuttler <yoshuakuttler@gmail.com>
2020-10-13 16:51:36 +0300

    update version skip-ci

---
+++ src/components/label/label.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, TouchableHighlight, Text} from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+});
+
+const Label = ({title}) => {
+  return <Text style={styles.text}>{title}!</Text>;
+};
+
+Label.propTypes = {
+  /**
+   * label title
+   */
+  title: PropTypes.string.isRequired,
+};
+
+export {Label};
